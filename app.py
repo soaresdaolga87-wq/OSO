536fc25c82bc64e21919f36eb87516cfa9bc572f
soaresdaolga87-wq <soaresdaolga87@gmail.com>
2025-08-28 21:41:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route("/pedido")
 def pedido():
-    # Aqui simulamos os dados que virão do usuário ou banco
     dados = {
         "nome": "João Soares",
         "bairro": "Namiteca",
@@ -14,4 +13,4 @@ def pedido():
     return render_template("pedido.html", **dados)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
